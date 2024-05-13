3c0045d6bfa0eb8e38e196acd33198cb57a7f4e1
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-05-13 17:50:03 +0300

    Merged streetview-panoid-bookmarklet

--- examples/basic/bookmarklet.js
+++ examples/basic/bookmarklet.js
@@ -12,23 +12,20 @@ for (var i=1; i<buttons.length; i++) {
 		j++;
 	}
 }
-var message = "PanoID: ";
 if (result) {
-	message = message + result[1] + '<br/><a href="https://freeali.se/PanomNom.js/examples/basic/sv-panoid.html?ids=' + ids.join(',') + '">load timepoints</a>';
+	window.location.href = 'https://freeali.se/PanomNom.js/examples/basic/sv-panoid.html?ids=' + ids.join(',');
 } else {
 	message = "PanoID not found."
+  messageEl.innerHTML = message;
+	body.appendChild(messageEl);
+	messageEl.style.zIndex = 2147483647;
+	messageEl.style.position = "fixed";
+	messageEl.style.top = "0";
+	messageEl.style.right = "0";
+	messageEl.style.fontFamily = "monospace";
+	messageEl.style.fontWeight = "bold";
+	messageEl.style.fontSize = "24px";
+	messageEl.style.backgroundColor = "#ffffff";
+	messageEl.style.color = '#000000';
+	messageEl.style.padding = "20px 20px 20px 20px";
 }
-
-messageEl.innerHTML = message;
-body.appendChild(messageEl);
-messageEl.style.zIndex = 2147483647;
-messageEl.style.position = "fixed";
-messageEl.style.top = "0";
-messageEl.style.right = "0";
-messageEl.style.fontFamily = "Arial, sans-serif";
-messageEl.style.fontWeight = "bold";
-messageEl.style.fontSize = "24px";
-messageEl.style.backgroundColor = "#f4ff60";
-messageEl.style.color = '#000000';
-messageEl.style.padding = "20px 20px 20px 20px";
-console.log(message);
