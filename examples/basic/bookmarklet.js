4c898ad21844abff97d429557fc9616b07195a6f
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-06-13 17:30:17 +0300

    Update bookmarklet.js

--- examples/basic/bookmarklet.js
+++ examples/basic/bookmarklet.js
@@ -4,9 +4,8 @@ var re = /https:\/\/www.google.com\/maps\/\S*data=\S*!1s(\S+)!2e/;
 var result = re.exec(window.location.href);
 var buttons = document.getElementsByTagName('button');
 var ids = [];
-ids[0] = result[1];
-var j = 1;
-for (var i=1; i<buttons.length; i++) {
+var j = 0;
+for (var i=0; i<buttons.length; i++) {
 	if (buttons[i].style.background.indexOf('panoid=') > -1) {
 		ids[j] = buttons[i].style.background.split('panoid=')[1].split('&')[0];
 		j++;
