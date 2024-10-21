2713ea84d11aa61fafdfe63efa10ab0ec7784af3
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-10-22 02:12:13 +0300

    Update bookmarklet.js

--- examples/basic/bookmarklet.js
+++ examples/basic/bookmarklet.js
@@ -4,17 +4,22 @@ var re = /https:\/\/www.google.com\/maps\/\S*data=\S*!1s(\S+)!2e/;
 var result = re.exec(window.location.href);
 var buttons = document.getElementsByTagName('button');
 var ids = [];
+var ps = [];
 var j = 0;
 for (var i=0; i<buttons.length; i++) {
-	if (buttons[i].style.background.indexOf('panoid=') > -1) {
-		ids[j] = buttons[i].style.background.split('panoid=')[1].split('&')[0];
-		j++;
-	}
+	 if (buttons[i].style.background.length) {
+	   if (buttons[i].style.background.indexOf('panoid=') >= 0) {
+		    ids[j] = buttons[i].style.background.split('panoid=')[1].split('&')[0];
+	   } else {
+					 ps[j] = buttons[i].style.background.split('/')[1].split('=')[0];
+				}
+		  j++;
+	 }
 }
 var url = 'https://freeali.se/panoramera/examples/basic/index.html';
-if (j>0) {
+if (ids.length>0) {
 	window.location.href = url + '?ids=' + ids.join(',');
-} else {
+} else if (ps.length>0) {
 	const l = window.location.href.split('@')[1].split('/')[0].split(',');
-	window.location.href = url + '?l=' + l[0] + ',' + l[1] 	+ '&c=' + encodeURIComponent(document.getElementById('titlecard').innerText) + '&ps=' + window.location.href.split('!1s')[1].split('!2e')[0];
+	window.location.href = url + '?l=' + l[0] + ',' + l[1] 	+ '&c=' + encodeURIComponent(document.getElementById('titlecard').innerText) + '&ps=' + ps.join(','); //+ window.location.href.split('!1s')[1].split('!2e')[0];
 }
