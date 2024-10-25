d6d73cab45cc90428e10a515a239bf75f0962468
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2024-10-25 17:51:42 +0300

    Update GooglePhotoSphereLoader.js

--- src/GooglePhotoSphereLoader.js
+++ src/GooglePhotoSphereLoader.js
@@ -31,7 +31,8 @@ class GooglePhotoSphereLoader extends Loader {
     this.panoId = id;
 
     const metadata = await getPhotoSphereInfo(id);
-    if (!metadata[1][0][4][1]) {
+    alert(JSON.stringify(metadata[1][0][4]));
+    if (metadata[1][0][4][1] === null) {
       const copyright = '';
     } else {
       const copyright = metadata[1][0][4][1][0][0][0];
